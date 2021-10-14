0655f45a034b86254445c078e6cbe31d49943ba5
Samuel Elias <samuel.elias.dev@gmail.com>
2021-10-14 20:11:51 -0300

    feat(core-src): coloca anotações para a entidade produto

--- core/src/main/java/domain/Products.java
+++ core/src/main/java/domain/Products.java
@@ -1,13 +1,22 @@
 package domain;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Products {
 
     @Id
@@ -25,4 +34,17 @@ public class Products {
 
     @NotEmpty(message = "the field name cannot be null")
     private String image;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Products products = (Products) o;
+        return id != null && Objects.equals(id, products.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
