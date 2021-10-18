aca5fbe3c75d897ae7ada66e5a6189c628b41dd3
Samuel Elias <samuel.elias.dev@gmail.com>
2021-10-18 00:29:36 -0300

    feat(core): adiciona o repositorio para applicationUser

---
+++ core/src/main/java/repository/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import domain.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
