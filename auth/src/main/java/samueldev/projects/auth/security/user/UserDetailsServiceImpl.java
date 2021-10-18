83e3904dfbd5cfc657b2730bd6d5bf36ec0969e8
Samuel Elias <samuel.elias.dev@gmail.com>
2021-10-18 00:35:51 -0300

    feat(src): adiciona uma implementação à userDetails com as informacoes de applicationUser

---
+++ auth/src/main/java/samueldev/projects/auth/security/user/UserDetailsServiceImpl.java
@@ -0,0 +1,66 @@
+package samueldev.projects.auth.security.user;
+
+import domain.ApplicationUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import repository.ApplicationUserRepository;
+
+import java.util.Collection;
+
+import static samueldev.projects.auth.utils.ValidateNullPointerEntities.validateIfEntityIsNull;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final ApplicationUserRepository applicationUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Searching in the database the user by username  '{}'", username);
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+        log.info("ApplicationUser found the '{}'", applicationUser);
+
+        validateIfEntityIsNull
+                (applicationUser, String.format("user '%s' was not found in the database", username));
+
+        return new CustomUserDetails(applicationUser);
+    }
+
+    private static final class CustomUserDetails extends ApplicationUser implements UserDetails {
+        CustomUserDetails(ApplicationUser applicationUser) {
+            super(applicationUser);
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_" + this.getRole());
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+}
