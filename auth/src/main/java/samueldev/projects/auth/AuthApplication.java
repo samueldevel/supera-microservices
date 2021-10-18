e050513b628caf873ce637862fbae33303b938d4
Samuel Elias <samuel.elias.dev@gmail.com>
2021-10-18 15:47:16 -0300

    fix(auth): muda o package do repositorio e do domain a ser lido

--- auth/src/main/java/samueldev/projects/auth/AuthApplication.java
+++ auth/src/main/java/samueldev/projects/auth/AuthApplication.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import property.JwtConfiguration;
+import samueldev.projects.core.property.JwtConfiguration;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = JwtConfiguration.class)
-@EntityScan({"domain"})
-@EnableJpaRepositories({"repository"})
+@EntityScan({"samueldev.projects.core.domain"})
+@EnableJpaRepositories({"samueldev.projects.core.repository"})
+@EnableEurekaClient
+@ComponentScan("samueldev.projects")
 public class AuthApplication {
-    
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
