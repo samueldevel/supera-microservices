07211d8e577ffed3867dba8765b834edc1a64e93
Samuel Elias <samuel.elias.dev@gmail.com>
2021-10-26 10:21:34 -0300

    feat(token-config): autoriza as url do swagger documentation

--- token/src/main/java/samueldev/projects/security/config/SecurityTokenConfig.java
+++ token/src/main/java/samueldev/projects/security/config/SecurityTokenConfig.java
@@ -1,6 +1,7 @@
 package samueldev.projects.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -27,6 +28,9 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
                 .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/swagger-resources/**", "/webjars/springfox-swagger-ui/**"
+                        , "/v2/api-docs/**").permitAll()
                 .antMatchers("/v1/products/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
