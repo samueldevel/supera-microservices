d4a28f2a8fbe43608bcf6d29779c3178ca40f35d
Samuel Elias <samuel.elias.dev@gmail.com>
2021-12-04 21:39:40 -0300

    feat(products/test/controller): create template to create max-min products

--- products/src/test/java/samueldev/projects/products/utils/CreateProducts.java
+++ products/src/test/java/samueldev/projects/products/utils/CreateProducts.java
@@ -1,6 +1,11 @@
 package samueldev.projects.products.utils;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import samueldev.projects.core.domain.Products;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CreateProducts {
 
@@ -27,8 +32,8 @@ public class CreateProducts {
         return Products.builder()
                 .id(1L)
                 .name("Call Of Duty Infinite Warfare II")
-                .price(49.99)
                 .score(80)
+                .price(49.99)
                 .image("call-of-duty-infinite-warfare.png")
                 .build();
     }
@@ -40,5 +45,21 @@ public class CreateProducts {
                 .score(score)
                 .image(image)
                 .build();
+    }
+
+    public static Page<Products> createProductByFilteringPriceByMinMax(int min, int max) {
+        PageImpl<Products> productsPage = new PageImpl<>(List.of(createValidProduct()));
+
+        List<Products> productFiltered = productsPage.stream().filter((products -> products.getPrice() > min && products.getPrice() < max)).collect(Collectors.toList());
+
+        return new PageImpl<>(productFiltered);
+    }
+
+    public static Page<Products> createProductByFilteringScoreByMinMax(int min, int max) {
+        PageImpl<Products> productsPage = new PageImpl<>(List.of(createValidProduct()));
+
+        List<Products> productFiltered = productsPage.stream().filter((products -> products.getScore() > min && products.getScore() < max)).collect(Collectors.toList());
+
+        return new PageImpl<>(productFiltered);
     }
 }
